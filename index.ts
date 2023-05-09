8a2e9e2b2dad8d421ee12c0bae488682d175a941
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2023-05-09 11:17:52 +0200

    added cors

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
 // Import the express in typescript file
 import express from 'express';
+import cors from "cors"
+
  
 // Initialize the express engine
 const app: express.Application = express();
+app.use(cors({ credentials: true, origin: true }))
  
 // Take a port 8080 for running server.
 const port: number = 8080;
  
 // Handling '/' Request
 app.get('/', (_req, _res) => {
-    _res.send("TypeScript With Express");
+    _res.send({"message":"TypeScript With Express"});
 });
  
 // Server setup
